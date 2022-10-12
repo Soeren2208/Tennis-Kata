b023d7737a4c3d525ec3ffa8c7eb920f8ce042f8
Sören Schwerk <s.schwerk@schule.bremen.de>
2022-10-12 19:22:55 +0200

    1 no Points played

--- src/main/java/de/szut/TennisGame.java
+++ src/main/java/de/szut/TennisGame.java
@@ -1,4 +1,12 @@
 package de.szut;
 
 public class TennisGame {
+
+    public TennisGame(String playerToServe, String playerToReturn){
+
+    }
+
+    public String getResult() {
+        return "Love all";
+    }
 }
--- src/test/java/de/szut/TennisGameTest.java
+++ src/test/java/de/szut/TennisGameTest.java
@@ -1,4 +1,25 @@
 package de.szut;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class TennisGameTest {
+
+    private TennisGame game;
+
+    @BeforeEach
+    public void setUp(){
+        this.game = new TennisGame("Federer", "Nadal");
+    }
+
+    @Test
+    public void givenNoPointPlay_WhenGetResult_ThenReturnLove(){
+        String result = this.game.getResult();
+        assertThat(result, is("Love all"));
+    }
+
+
 }
